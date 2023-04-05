d3b9aa7fd2619993be04f13147b6f127c192893b
VictorBelotto <victortucunduva.vb@outlook.com>
2023-04-04 21:56:35 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,22 @@
+const financiamento = document.querySelector("#input_financiamento")
+const prazo = document.querySelector("#input_prazo")
 const parcela = document.querySelector("#input_parcela")
-const juros = document.querySelector("#input_parcela")
+const juros = document.querySelector("#input_juros")
+const botao = document.querySelector("#botao_preencher")
+
+botao.addEventListener('click', ()=>{
+    calculaRenda(financiamento.valueAsNumber, prazo.valueAsNumber, juros.valueAsNumber)
+
+})
+
+function calculaRenda(financiamento, prazo, juros){
+    let jurosMensal = juros / 12
+    let parcelaSemJuros = financiamento / prazo
+
+    let calc = (parcelaSemJuros * jurosMensal) + (parcelaSemJuros)
+
+    let rendaBruta = (calc * 100) / 30
+
+    console.log(rendaBruta)
+
+}
